2174687feb6b37911e2493133358a24d86c6828d
Michal Ziulek <michalziulek@gmail.com>
2017-11-01 17:05:11 +0100

    Add files via upload

--- ShaderV.glsl
+++ ShaderV.glsl
@@ -1,22 +1,22 @@
 float Plot(vec2 st, float pct)
 {
-    return smoothstep(pct - 0.02, pct, st.y) - smoothstep(pct, pct + 0.02, st.y);
+  return smoothstep(pct - 0.02, pct, st.y) - smoothstep(pct, pct + 0.02, st.y);
 }
 
 void main()
 {
-    float time = gl_TexCoord[0].s * 0.001;
+  float time = gl_TexCoord[0].s * 0.001;
 
-    vec2 st = -1.0 + 2.0 * (gl_FragCoord.xy / vec2(1280, 720));
+  vec2 st = -1.0 + 2.0 * (gl_FragCoord.xy / vec2(1280, 720));
 
-    float y = cos(sin(4.0 * st.x + time)) - 0.5;
+  float y = cos(sin(4.0 * st.x + time)) - 0.5;
 
-    float pct = Plot(st, y);
+  float pct = Plot(st, y);
 
-    vec3 colorA = vec3(0.149, 0.141, 0.912);
-    vec3 colorB = vec3(1.000, 0.833, 0.224);
+  vec3 colorA = vec3(0.149, 0.141, 0.912);
+  vec3 colorB = vec3(1.000, 0.833, 0.224);
 
-    vec3 color = pct * mix(colorA, colorB, y);
+  vec3 color = pct * mix(colorA, colorB, y);
 
-    gl_FragColor = vec4(color, 1.0);
+  gl_FragColor = vec4(color, 1.0);
 }
